1984b3bcf080d03208cf982d75ba7587447adf5e
NourSarikaya <112666078+NourSarikaya@users.noreply.github.com>
2025-01-12 13:07:46 -0600

    Create Lambdas.java

---
+++ Lambdas.java
@@ -0,0 +1,14 @@
+public class Lambdas{
+	public static void main(String[] args){
+    Cat myCat= new Cat();
+    myCat.print(); //we can do this
+
+    //OR
+
+    printThing(myCat); //Calling method on Printable thing
+    }
+
+    static void printThing(Printable thing){
+	thing.print();
+    }
+}
